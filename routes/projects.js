17200be25b6134b3697e15f60cbf5b995037ad0c
mattcosta617 <mattiecosta@gmail.com>
2020-08-27 19:17:41 -0400

    User Verifications

--- routes/projects.js
+++ routes/projects.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/project');
-const authRequired = require('../middleware/authRequired');
+// const authRequired = require('../middleware/authRequired');
 
 router.get('/', ctrl.index);
 router.get('/:id', ctrl.show);
-router.post('/', authRequired, ctrl.create);
-router.put('/:id', authRequired, ctrl.update);
-router.delete('/:id', authRequired, ctrl.destroy);
+router.post('/', ctrl.create);
+router.put('/:id', ctrl.update);
+router.delete('/:id', ctrl.destroy);
 
 module.exports = router;
